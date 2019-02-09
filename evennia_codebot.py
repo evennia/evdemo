513e441b5356834249d1deac91832c2ecfc99970
Griatch <griatch@gmail.com>
2019-02-09 10:30:09 +0100

    Cleanup of reformat, try add space

--- evennia_codebot.py
+++ evennia_codebot.py
@@ -105,12 +105,13 @@ def fmt_path(msg):
 
 def fmt_crop(text, length=60):
     "Crop text to given length and remove line breaks inside text"
-    text = re.sub(r"\n\r|\r\n|\r|\n", r"\\", text.strip())
     nlen = len(text)
+    text = re.sub(r"\n\r|\r\n|\r|\n", r"\\ ", text.strip())
     diff = nlen - length
     if nlen > length:
         text = "{txt}{postfix}".format(
             txt=text[:length],
+            txt.strip("\ "),
             postfix=clr("[{} more]".format(diff) if diff > 10 else "[...]", 'grey'))
     return text
 
